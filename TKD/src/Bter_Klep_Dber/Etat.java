6d8770657b07bb83cca7c5b3465b38d529106b70
Boris <boboshesam@gmail.com>
2015-04-14 00:02:36 +0200

    actu

---
+++ TKD/src/Bter_Klep_Dber/Etat.java
@@ -0,0 +1,15 @@
+package Bter_Klep_Dber;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zirend
+ */
+public enum Etat {
+    INIT, JOUEUR, ROUTE;
+}
